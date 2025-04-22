102a3ff9d606767f39add2fa0b87ada0f65b8908
Vishwanath <vishwanath9216@gmail.com>
2025-04-22 21:30:16 +0530

    reset password added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   check,
+  forgotPassword,
+  getAllUsers,
   login,
   logout,
   register,
+  resetPassword,
   verifyUser,
 } from "../controller/auth.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
@@ -18,5 +21,11 @@ authRoute.post("/login", login);
 authRoute.post("/logout", authMiddleware, logout);
 
 authRoute.get("/check", authMiddleware, check);
+
+authRoute.post("/fogotpassword",forgotPassword)
+
+authRoute.post("/resetpassword/:token/:userId",resetPassword)
+
+authRoute.get("/getAllUsers",getAllUsers)
 
 export default authRoute;
