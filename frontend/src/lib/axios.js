695f52efcec86443d14a80038ea26f5a437bd300
Vishwanath <vishwanath9216@gmail.com>
2025-05-25 23:19:17 +0530

    added backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
-      : "http://localhost:8080/api/v1",
+      : "https://leetlab-backend-d65o.onrender.com",
   withCredentials: true,
 });
